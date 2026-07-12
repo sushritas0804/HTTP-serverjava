c649c26e256498ffd64af4018073dfe0555da0e2
sushritas0804 <sushritas8406@gmail.com>
2026-07-12 23:04:35 +0530

    changing the import packages

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 public class SimpleHTTPServer {
     public static void main(String[]args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
