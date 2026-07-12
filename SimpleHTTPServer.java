4ec4bbe650b97a8338d4dfcec9560e3ab1a87751
sushritas0804 <sushritas8406@gmail.com>
2026-07-12 21:19:32 +0530

    InputStreamReader bridge

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 public class SimpleHTTPServer {
     public static void main(String[]args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080.....");
         while(true){
-            final Socket client = server.accept();
+             Socket clientSocket = server.accept();
+             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
         }
     }
 }
