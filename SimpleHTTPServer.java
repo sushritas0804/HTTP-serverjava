eed6fee5de73b9fbb6e7312939bade807cd61c83
sushritas0804 <sushritas8406@gmail.com>
2026-07-12 23:08:34 +0530

    eheulfefhe

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -4,16 +4,11 @@ import java.net.Socket;
 import java.util.Date;
 public class SimpleHTTPServer {
     public static void main(String[]args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080.....");
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080...");
         while(true){
-             Socket clientSocket = server.accept();
-             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-             BufferedReader reader = new BufferedReader(isr);
-             String line = reader.readLine();
-             while(!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            
+        }
              }
         }
     }
