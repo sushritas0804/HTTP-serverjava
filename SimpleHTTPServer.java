a5ae4de16cebe35865adf76e3e5b876c32c787d8
sushritas0804 <sushritas8406@gmail.com>
2026-07-12 23:14:42 +0530

    final commit - sending a response back to client from HTTP server

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -7,9 +7,11 @@ public class SimpleHTTPServer {
         ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080...");
         while(true){
-            
+            try(Socket socket = server.accept()){
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            }
         }
-             }
-        }
-    }
+     }
 }
